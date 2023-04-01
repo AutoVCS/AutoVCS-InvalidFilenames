88cd679ce49a6b05f66a84a0f0f6455ae510d61b
Kai Presler-Marshall <kai@presler-marshall.org>
2023-04-01 18:15:00 -0400

    Initial commit, with valid and invalid files

---
+++ MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass {
+
+	static public void main (String args[]){
+
+		System.out.println("This is a Java file, which we'd like to be able to analyse, even though their are invalid files in this repository, too");
+	}
+
+}
